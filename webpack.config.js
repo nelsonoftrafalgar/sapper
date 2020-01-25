1faf6f0ddca1443363ae553f10dcb3083d572ebf
Michal Mancewicz <nelsonoftrafalgar@gmail.com>
2020-01-25 16:02:54 +0100

    add setup

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: ['./src/index.ts'],
+  resolve: {
+    extensions: ['.ts', '.js'],
+    modules: ['node_modules']
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.min.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
